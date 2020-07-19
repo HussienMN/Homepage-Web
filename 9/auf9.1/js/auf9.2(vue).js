55ad91fccb41feec08b4baf88778b5217c3feeab
Unknown <unknown@example.com>
2020-07-19 15:16:45 +0300

    Lösungen der Aufgabe 9

---
+++ 9/auf9.1/js/auf9.2(vue).js
@@ -0,0 +1,14 @@
+Vue.component('side-menue', {
+    data: function () {
+      return {
+        count: 0
+      }
+    },
+    template: '<button v-on:click="count++">You clicked me {{ count }} times.</button>'
+  })
+  var = new Vue({
+    el: "#replace"
+  })
+
+
+  
